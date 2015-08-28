d346ef6ac2f7dafb1783f62d869619156a64c7b3
stevenlundy <steven.lundy@gmail.com>
2015-08-28 14:26:22 -0700

    Wrote Enemy Class and Player Class. Wrote updateEnemy function

--- client/watchout.js
+++ client/watchout.js
@@ -1,5 +1,15 @@
 // start slingin' some d3 here.
+var Enemy = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 10;
+};
 
+var Player = function() {
+  this.x = window.board.width/2;
+  this.y = window.board.height/2;
+  this.r = 10;
+};
 
 var init = function(){
   window.board = {
@@ -16,9 +26,47 @@ var init = function(){
     .attr('height', window.board.height);
 
   for (var i = 0; i < window.board.numEnemies; i++) {
-    window.board.enemies.push(new Enemy());
+    var x = randomBetween(1, board.width);
+    var y = randomBetween(1, board.height);
+    window.board.enemies.push(new Enemy(x, y));
   }
+  updateEnemies();
+
   for (var i = 0; i < window.board.numPlayers; i++) {
     window.board.players.push(new Player());
   }
 };
+
+var updateEnemies = function(){
+  // DATA JOIN
+  var enemies = board.svg.selectAll('circle').data(board.enemies);
+
+  // UPDATE
+  enemies.transition().duration(1000)
+    .attr('cy', function(d) { 
+      d.y = randomBetween(1, window.board.height);
+      return d.y;
+    })
+    .attr('cx', function(d) { 
+      d.x = randomBetween(1, window.board.width);
+      return d.x;
+    });
+
+  // ENTER
+  enemies.enter().append('circle')
+    .attr('cy', function(d) { return d.y })
+    .attr('cx', function(d) { return d.x })
+    .attr('r', function(d) { return d.r });
+
+  // ENTER + UPDATE
+
+  // EXIT
+
+  setTimeout(updateEnemies, 1000);
+};
+
+var randomBetween = function(min, max) {
+  return Math.floor(Math.random()*(max-min) + min);
+};
+
+init();
